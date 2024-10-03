a5ae853b518b5643dd7d5661324c462358970c75
Caique Rodrigues <142418237+Caique0Og@users.noreply.github.com>
2024-10-03 17:28:20 -0300

    Add files via upload

---
+++ 05032024/ifElse.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace idade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Estrutura de If");
+
+            int x;
+
+            x = Convert.ToInt32(Console.ReadLine());
+            if (x >= 18)
+            {
+                if (x >= 60)
+                {
+                    Console.WriteLine("idoso");
+
+                }
+                else
+                {
+                    Console.WriteLine("maior de idade");
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("criança");
+
+            }Console.ReadKey();
+        
+        } 
+            
+        
+
+    }
+}
